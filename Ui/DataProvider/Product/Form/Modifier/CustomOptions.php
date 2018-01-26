1cd4e827ec0e6f5862dea1515fdd4edb71cb2c58
Jan Brinkmann <mail@janbrinkmann.de>
2018-01-26 12:29:35 +0100

    missing use statements lead to class ... not found

--- Ui/DataProvider/Product/Form/Modifier/CustomOptions.php
+++ Ui/DataProvider/Product/Form/Modifier/CustomOptions.php
@@ -3,6 +3,24 @@
 namespace Luckyduck\NegativeOptionValue\Ui\DataProvider\Product\Form\Modifier;
 
 use Magento\Catalog\Ui\DataProvider\Product\Form\Modifier\CustomOptions as Options;
+use Magento\Catalog\Model\Locator\LocatorInterface;
+use Magento\Store\Model\StoreManagerInterface;
+use Magento\Catalog\Model\ProductOptions\ConfigInterface;
+use Magento\Catalog\Model\Config\Source\Product\Options\Price as ProductOptionsPrice;
+use Magento\Framework\UrlInterface;
+use Magento\Framework\Stdlib\ArrayManager;
+use Magento\Ui\Component\Modal;
+use Magento\Ui\Component\Container;
+use Magento\Ui\Component\DynamicRows;
+use Magento\Ui\Component\Form\Fieldset;
+use Magento\Ui\Component\Form\Field;
+use Magento\Ui\Component\Form\Element\Input;
+use Magento\Ui\Component\Form\Element\Select;
+use Magento\Ui\Component\Form\Element\Checkbox;
+use Magento\Ui\Component\Form\Element\ActionDelete;
+use Magento\Ui\Component\Form\Element\DataType\Text;
+use Magento\Ui\Component\Form\Element\DataType\Number;
+use Magento\Framework\Locale\CurrencyInterface;
 
 class CustomOptions extends Options
 {
