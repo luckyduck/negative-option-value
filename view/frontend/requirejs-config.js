3736a97e57e2725838729f6ead587477b81b8780
Jan Brinkmann <mail@janbrinkmann.de>
2018-01-26 22:09:42 +0100

    fix for drop-down

---
+++ view/frontend/requirejs-config.js
@@ -0,0 +1,9 @@
+var config = {
+    config: {
+        mixins: {
+            'Magento_Catalog/js/price-options': {
+                'Luckyduck_NegativeOptionValue/js/price-options-mixin': true
+            }
+        }
+    }
+};
